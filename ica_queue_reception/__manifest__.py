a6932098ec31e21f72bf96fd122a041ff7063f8a
Agga <agga@Aggas-MacBook-Air.local>
2024-06-04 21:56:09 +0700

    [add] window action and menu

--- ica_queue_reception/__manifest__.py
+++ ica_queue_reception/__manifest__.py
@@ -2,5 +2,10 @@
     "name": "Queue Reception",
     "author":"IdeaCode Academy",
     "depends": ["base"],
+    "data":[
+        "views/ica_queue_reception.xml",
+
+        "views/menus.xml",
+    ],
     "license": "LGPL-3",
 }
