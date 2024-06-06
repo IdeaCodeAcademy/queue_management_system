2343deb6aaa087c33ae22d0e4ad03a79f98ff618
Agga <agga@Aggas-MacBook-Air.local>
2024-06-06 23:13:35 +0700

    [add] state

--- ica_queue_reception/models/ica_queue_reception.py
+++ ica_queue_reception/models/ica_queue_reception.py
@@ -8,9 +8,17 @@ class IcaQueueReception(models.Model):
     name = fields.Char(string="Name", readonly=True, default=lambda x: _('New'))
     partner_id = fields.Many2one("res.partner", string="Partner",required=True)
     counter_id = fields.Many2one("ica.queue.counter", string="Counter",required=True)
+    state = fields.Selection([('draft','Draft'),('confirm','Confirm')],default="draft")
 
     @api.model
     def create(self, values):
         if values.get('name', _('New')) == _("New"):
             values['name'] = self.env['ir.sequence'].next_by_code('ica.queue') or _("New")
         return super(IcaQueueReception, self).create(values)
+
+
+    def action_confirm(self):
+        self.state = 'confirm'
+
+    def action_draft(self):
+        self.state = 'draft'
