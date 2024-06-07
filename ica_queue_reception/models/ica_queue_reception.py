6c53a4d30ee62aa080f58b97369499a2a66e5578
Agga <agga@Aggas-MacBook-Air.local>
2024-06-08 00:17:31 +0700

    [add] queue reception

--- ica_queue_reception/models/ica_queue_reception.py
+++ ica_queue_reception/models/ica_queue_reception.py
@@ -6,18 +6,19 @@ class IcaQueueReception(models.Model):
     _name = "ica.queue.reception"
     _description = "Ica Queue Reception"
 
-    name = fields.Char(string="Name", readonly=True, default=lambda x: _('New'))
+    name = fields.Char(string="Reference", readonly=True, default=lambda x: _('New'))
     partner_id = fields.Many2one("res.partner", string="Partner",required=True)
     counter_id = fields.Many2one("ica.queue.counter", string="Counter",required=True)
     state = fields.Selection([('draft','Draft'),('confirm','Confirm')],default="draft")
 
-    @api.model
-    def create(self, values):
-        if values.get('name', _('New')) == _("New"):
-            values['name'] = self.env['ir.sequence'].next_by_code('ica.queue') or _("New")
-        return super(IcaQueueReception, self).create(values)
+    # @api.model
+    # def create(self, values):
+    #
+    #     return super(IcaQueueReception, self).create(values)
 
     def action_confirm(self):
+        if self.name == _("New"):
+            self.name = self.env['ir.sequence'].next_by_code('ica.queue') or _("New")
         self.change_state('confirm')
         # self.state = 'confirm'
 
