bc6594ca0c2a413d950edc5784f73ced472ca71d
Agga <agga@Aggas-MacBook-Air.local>
2024-06-09 20:21:23 +0700

    [fix] cashier

--- ica_queue_reception/models/ica_queue_cashier.py
+++ ica_queue_reception/models/ica_queue_cashier.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class IcaQueueCashier(models.Model):
     _inherit = 'ica.queue.cashier'
 
-    reception_id = fields.Many2one('ica.queue.reception', readonly=1)
+    reception_id = fields.Many2one('ica.queue.reception', readonly=True)
