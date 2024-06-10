43055c8a0a94f9842fa2745d8108dd8241a4eaed
Agga <agga@Aggas-MacBook-Air.local>
2024-06-11 00:18:20 +0700

    [add] routerormservice

--- ica_queue_reception/static/src/reception_kiosk/reception_kiosk.js
+++ ica_queue_reception/static/src/reception_kiosk/reception_kiosk.js
@@ -1,23 +1,31 @@
 /** @odoo-module **/
 
-import {Component,useState} from "@odoo/owl";
+import {Component, useState} from "@odoo/owl";
 import {registry} from "@web/core/registry";
 
 class ReceptionKiosk extends Component {
-    setup(){
+    async setup() {
+        this.routerService = this.env.services.router;//useService('router');
+        this.ormService = this.env.services.orm;
         this.state = useState({
-            "counter": this.getCounter(),
+            "counter":{},
         })
-        console.log();
+         await this.getCounter();
     }
 
-    getCounter(){
-        const counter = this.props.action.context.counter;
-        if(counter != undefined){
-            return counter;
-        }else{
-            // return ;
+    async getCounter() {
+        var counter = this.props.action.context.counter;
+        let current = this.routerService.current;
+        if (counter !== undefined) {
+            current.search.counter_id = counter.id
+        } else {
+            if (current.search.counter_id) {
+                let counters = await this.ormService.searchRead('ica.queue.counter',[['id','=',current.search.counter_id]],
+                    ['id','name','type'])
+                counter = counters[0];
+            }
         }
+        this.state.counter = counter;
     }
 }
 
